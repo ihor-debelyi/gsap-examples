2af20172b792443c1c4e93755f8df28c1bf84ceb
Ihor Debelyi <ihor.debelyi@gmail.com>
2026-01-04 16:43:34 +0100

    added other gsap code examples

---
+++ CGMWTOCT2024/Source Code/alex-finley/content/data.js
@@ -0,0 +1,127 @@
+export const carouselItems = [
+  {
+    id: "01",
+    title: "Visionary",
+    tags: ["digital art, innovation, branding, visuals"],
+    bg: "../assets/carousel/bg-1.jpg",
+    main: "../assets/carousel/main-1.jpg",
+    url: "./project.html",
+  },
+  {
+    id: "02",
+    title: "Urban Pulse",
+    tags: ["frontend, illustration, interface, showcase"],
+    bg: "../assets/carousel/bg-2.jpg",
+    main: "../assets/carousel/main-2.jpg",
+    url: "./project.html",
+  },
+  {
+    id: "03",
+    title: "Golden Hour",
+    tags: ["reel, videography, platform, promotions"],
+    bg: "../assets/carousel/bg-3.jpg",
+    main: "../assets/carousel/main-3.jpg",
+    url: "./project.html",
+  },
+  {
+    id: "04",
+    title: "Aero Lens",
+    tags: ["software, photo gallery, portal, UX design"],
+    bg: "../assets/carousel/bg-4.jpg",
+    main: "../assets/carousel/main-4.jpg",
+    url: "./project.html",
+  },
+  {
+    id: "05",
+    title: "Motion Craft",
+    tags: ["film, trailer, platform, marketing"],
+    bg: "../assets/carousel/bg-5.jpg",
+    main: "../assets/carousel/main-5.jpg",
+    url: "./project.html",
+  },
+];
+
+export const services = [
+  {
+    id: "01",
+    title: "Web Design",
+    copy: "I focus on building visually engaging and highly functional websites that create a lasting impact. With a consistent visual theme, each design is crafted to complement your brand and connect with your audience.",
+  },
+  {
+    id: "02",
+    title: "Webflow Development",
+    copy: "As a Webflow Partner, I utilize this powerful platform to create efficient, visually stunning sites that meet your unique requirements, ensuring each project is user-friendly, scalable, and visually compelling.",
+  },
+  {
+    id: "03",
+    title: "Animation & Interaction",
+    copy: "Adding animations and interactions brings depth to the digital experience. From subtle transitions to intricate animations, I use modern tools to design elements that capture attention and enhance engagement.",
+  },
+  {
+    id: "04",
+    title: "Art Direction",
+    copy: "Blending innovative AI-driven image creation with creative vision, I develop distinctive visuals that resonate with audiences. My goal is to craft storytelling visuals that leave a lasting impression and drive engagement.",
+  },
+];
+export const archive = [
+  {
+    name: "Shift in Design",
+    year: "2024",
+    images: ["img1"],
+  },
+  {
+    name: "Evelyn",
+    year: "2021",
+    images: ["img2"],
+  },
+  {
+    name: "Echo",
+    year: "2022",
+    images: ["img3", "img4", "img5"],
+  },
+  {
+    name: "Beyond Borders",
+    year: "2024",
+    images: ["img6", "img7", "img8", "img9"],
+  },
+  {
+    name: "Artifact",
+    year: "2020",
+    images: ["img10", "img11", "img12"],
+  },
+  {
+    name: "Legacy Showcase",
+    year: "2021",
+    images: ["img13", "img14", "img15"],
+  },
+  {
+    name: "Waveform",
+    year: "2019",
+    images: ["img16", "img17", "img18", "img19"],
+  },
+  {
+    name: "Axis",
+    year: "2024",
+    images: ["img20", "img21", "img22", "img23"],
+  },
+  {
+    name: "Capsule",
+    year: "2023",
+    images: ["img24", "img25", "img26"],
+  },
+  {
+    name: "Cobalt Collective",
+    year: "2018",
+    images: ["img27", "img28", "img29", "img30"],
+  },
+  {
+    name: "Nightfall",
+    year: "2021",
+    images: ["img31", "img32", "img33"],
+  },
+  {
+    name: "Sprout",
+    year: "2017",
+    images: ["img34", "img35"],
+  },
+];
